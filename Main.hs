8aea1298d2fb96d811e9758b52cfb94e7c0f50a3
Ankit Tibrewal <ankitibrewal@gmail.com>
2015-02-14 22:55:58 +0800

    Handling Round 2

--- Main.hs
+++ Main.hs
@@ -6,7 +6,7 @@ import Data.Monoid (mconcat)
 import Data.Char
 import Prelude as P
 import Text.Regex.Posix
-
+import Math.NumberTheory.Powers
 
 main = scotty 4000 $ do
     middleware logStdoutDev
@@ -20,9 +20,10 @@ processQuery :: D.Text -> Int
 processQuery x
   | D.unpack x =~ D.unpack "which of the following numbers is the largest:*" = P.maximum (convtToIntArray x)
   | D.unpack x=~ D.unpack "what is [0-9]* plus [0-9]*"  ::Bool = sum (convtToIntArray x)
+  | D.unpack x=~ D.unpack "what is [0-9]* multiplied by [0-9]*"  ::Bool = product (convtToIntArray x)
+  | D.unpack x=~ D.unpack "which of the following numbers is both a square and a cube: [0-9]*"  ::Bool = findSqRootCubeRoot (convtToIntArray x)
   |otherwise = -1
   
-
 
 convtToIntArray:: Text->[Int]
 convtToIntArray x= convtStringArrToIntArr . P.filter (P.all isDigit) . 
@@ -32,3 +33,6 @@ convtToIntArray x= convtStringArrToIntArr . P.filter (P.all isDigit) .
 
 convtStringArrToIntArr::[String]->[Int]
 convtStringArrToIntArr = P.map read 
+
+findSqRootCubeRoot::[Int]->Int
+findSqRootCubeRoot x=P.head $ P.filter (\y -> isCube y && isSquare y) $ x
